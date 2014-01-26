8bd34861c39a48d98046db0f76f7ad92aad958ef
Ubuntu <ubuntu@hpquiz.novalocal>
2014-01-26 12:27:55 +0000

    testing

--- divide/divide.py
+++ divide/divide.py
@@ -1,7 +1,9 @@
 import subprocess
 import os
 split_pages = 1
-hashDelimiter = "#################################################"
+hashDelimiter = "\n#################################################\n"
+fo = open('split_pages.txt', 'wb')
+fo.close()
 for i in range(1,15):
 	p = subprocess.Popen(['pdf2txt.py', '-p',str(i),'carlo.pdf'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out, err = p.communicate()
@@ -10,12 +12,12 @@ for i in range(1,15):
 		if out.find('The New York Times Company. All Rights Reserved.') != -1:
 			print 'New page'
 			# Open a file
-			fo = open("split_pages.txt", "wb")
-			fo.write(hashDelimiter)
-			fo.write(out)
-			split_pages = split_pages+1
-			# Close opend file
-			fo.close()
+			with open("split_pages.txt", "a") as fo:
+				fo.write(hashDelimiter)
+				fo.write(out)
+				split_pages = split_pages+1
+				# Close opend file
+				fo.close()
 		else:
 			print 'Previos Page'
 			split_pages =split_pages-1
@@ -23,3 +25,8 @@ for i in range(1,15):
 				myfile.write(out)
 
 
+fileReader = open('split_pages.txt')
+full_paper = fileReader.read()
+split_paper = full_paper.split('\n#################################################\n')
+for i in range(len(split_paper)):
+	print split_paper[i].split('The New York Times\n')[0].split('\n')[-5:-1]
