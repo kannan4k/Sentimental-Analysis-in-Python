bb8db76f0ebf2844812e0359e06dc7421fe622f7
Ubuntu <ubuntu@hpquiz.novalocal>
2014-01-26 12:27:55 +0000

    testing

--- divide/divide.py
+++ divide/divide.py
@@ -1,7 +1,9 @@
 import subprocess
 import os
 split_pages = 1
-hashDelimiter = "#################################################"
+hashDelimiter = "\n#################################################\n"
+fo = open('split_pages.txt', 'wb')
+fo.close()
 for i in range(1,15):
 	p = subprocess.Popen(['pdf2txt.py', '-p',str(i),'carlo.pdf'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out, err = p.communicate()
@@ -10,12 +12,12 @@ for i in range(1,15):
 		if out.find('The New York Times Company. All Rights Reserved.') != -1:
 			print 'New page'
 			# Open a file
-			fo = open("split_pages.txt", "wb")
-			fo.write(hashDelimiter)
-			fo.write(out)
-			split_pages = split_pages+1
-			# Close opend file
-			fo.close()
+			with open("split_pages.txt", "a") as fo:
+				fo.write(hashDelimiter)
+				fo.write(out)
+				split_pages = split_pages+1
+				# Close opend file
+				fo.close()
 		else:
 			print 'Previos Page'
 			split_pages =split_pages-1
